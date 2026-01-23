81cb778570d168c048c318d9883178bd3aef9d9c
SachinSharma05 <sachin.sharma0583@gmail.com>
2026-01-23 12:24:21 +0530

    Vercel deployment fix-21

--- apps/ui/hooks/useClients.ts
+++ apps/ui/hooks/useClients.ts
@@ -6,6 +6,6 @@ export function useClients() {
     queryKey: ['clients'],
     queryFn: getClients,
     staleTime: 1000 * 60 * 10, // 10 minutes
-    cacheTime: 1000 * 60 * 30, // 30 minutes
+    gcTime: 1000 * 60 * 30,    // 30 minutes (Renamed from cacheTime)
   });
 }
